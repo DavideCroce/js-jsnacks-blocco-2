764594c0aac5b3663727d3fad3eb6356a3171167
IskahnX <davidecroce10@gmail.com>
2022-01-13 17:30:52 +0100

    starting jsnack 5

---
+++ snack 5/js/script5.js
@@ -0,0 +1,8 @@
+console.log ('JS OK');
+
+/*
+Crea due array che hanno un numero di elementi diversi.
+aggiungi elementi casuali all'array che ha meno elementi,
+fino a quando ne avrà tanti quanti l'altro.
+*/
+
