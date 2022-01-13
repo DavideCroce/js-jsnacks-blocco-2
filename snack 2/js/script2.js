04bfda9a15c5880223775e304c60ffe3fe46bc25
IskahnX <davidecroce10@gmail.com>
2022-01-13 16:08:22 +0100

    cicle while jsnack 2 done

--- snack 2/js/script2.js
+++ snack 2/js/script2.js
@@ -5,3 +5,16 @@ Inserisci un numero, se è pari stampa il numero,
 se è dispari stampa il numero successivo
 */
 
+let numb = parseInt(prompt('Write a number'));
+
+while (numb % 2 == 0) {
+    if (numb % 2 == 0 ){
+        console.log (numb)
+    }
+    else {
+        numb = parseInt(prompt('Write a number'));
+    }
+}
+
+console.log (numb);
+
