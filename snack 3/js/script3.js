d04f2f440ab666aca3737c9dc6c04057416ef8f3
IskahnX <davidecroce10@gmail.com>
2022-01-13 16:30:11 +0100

    cicle while jsnack 3 done

--- snack 3/js/script3.js
+++ snack 3/js/script3.js
@@ -6,3 +6,22 @@ e una lista di cognomi,
 Gatsby vuole generare una falsa lista di 3 inviati.
 */
 
+const fake = [];
+const FirstN = ['Luigi' , 'Matteo' , 'Roberto' , 'Davide' ];
+const LastN = ['Rossi' , 'Bianchi' , 'Verdi' , 'Inzaghi'];
+
+while (fake.length < 3){
+    const rnd1 = Math.floor(Math.random() * FirstN.length);
+    const rnd2 = Math.floor(Math.random() * LastN.length);
+
+    const nome = FirstN[rnd1];
+    const last = LastN[rnd2];
+
+    const fakeP =  ` ${nome} ${last} `;
+
+    if(!fake.includes (fakeP)){
+        fake.push (fakeP);
+    }
+}
+
+console.log (fake);
