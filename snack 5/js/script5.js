9f1428facaf7af7ec9d759b131987fad2e0c6fe5
IskahnX <davidecroce10@gmail.com>
2022-01-13 17:57:54 +0100

    cicle while jsnack 5 done

--- snack 5/js/script5.js
+++ snack 5/js/script5.js
@@ -6,3 +6,18 @@ aggiungi elementi casuali all'array che ha meno elementi,
 fino a quando ne avrà tanti quanti l'altro.
 */
 
+const ShortA = [ 5 , 14 , 6];
+const LongA = [ 12 , 3 , 25 , 88 , 36];
+let numb = 0;
+
+while (ShortA.length = LongA.length){
+    if(ShortA.length < LongA.length){
+        ShortA.push (Math.floor(Math.random(numb) * 100) + 1);
+        console.log(numb);
+    }
+    numb++;
+}
+
+console.log(ShortA);
+console.log(LongA);
+console.log(numb);
