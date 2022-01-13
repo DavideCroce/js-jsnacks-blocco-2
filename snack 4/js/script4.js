19bb8fe85e2cc2d1e296bb36f0229d2de7527e61
IskahnX <davidecroce10@gmail.com>
2022-01-13 17:15:39 +0100

    cicle while jsnak 4 done

--- snack 4/js/script4.js
+++ snack 4/js/script4.js
@@ -5,14 +5,18 @@ Crea un array di numeri interi
 e fai la somma di tutti gli elementi che sono in posizipone dispari.
 */
 
-const numb = [ '12' , '55' , '29' , '34' , '84' , '9'];
+const numb = [ 12 , 55 , 29, 34 , 84 , 9];
+let pos = 0;
+let somma = 0;
+while (pos < numb.length){
 
-while (!(numb.length % 2 == 0)){
-
-    if (!(numb.length % 2 == 0)){
-        console.log (numb);
+    if (pos % 2 != 0){
+        console.log (numb[pos]);
+        somma += numb[pos];
     }
+
+    pos++;
 }
 
-console.log (numb);
+console.log (somma);
 
