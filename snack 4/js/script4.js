4af78b096de8e1123114326d3968cb4e2d29de1e
IskahnX <davidecroce10@gmail.com>
2022-01-13 17:03:12 +0100

    jsnack 4

--- snack 4/js/script4.js
+++ snack 4/js/script4.js
@@ -5,3 +5,14 @@ Crea un array di numeri interi
 e fai la somma di tutti gli elementi che sono in posizipone dispari.
 */
 
+const numb = [ '12' , '55' , '29' , '34' , '84' , '9'];
+
+while (!(numb.length % 2 == 0)){
+
+    if (!(numb.length % 2 == 0)){
+        console.log (numb);
+    }
+}
+
+console.log (numb);
+
